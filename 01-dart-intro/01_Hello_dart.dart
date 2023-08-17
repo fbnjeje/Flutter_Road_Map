6554ef6d5430ebfe2c6bae937bf0215b16029996
Neonx <fabiangomes2015@gmail.com>
2023-08-17 09:46:49 -0500

    first Hello World on Dart

---
+++ 01-dart-intro/01_Hello_dart.dart
@@ -0,0 +1,10 @@
+void main (){
+  //var myName = 'ferxxxo';
+  // late final myName;
+  //String myName = 'ferxxoo';
+
+Const myName = 'ferxxxo'
+print('Hello $myName');
+print('Hello ${myName.toUpperCase() }');
+print('Hola ${ 1 + 1 }');
+}
