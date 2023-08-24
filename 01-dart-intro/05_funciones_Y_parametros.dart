7240755c32853d15d1b41a270e182c7cfb604213
Neonx <fabiangomes2015@gmail.com>
2023-08-24 16:30:40 -0500

    parametros con nombre

--- 01-dart-intro/05_funciones_Y_parametros.dart
+++ 01-dart-intro/05_funciones_Y_parametros.dart
@@ -2,6 +2,8 @@ void main (){
   print(greetEveryOne());
   
   print('suma ${addTwoNumbersOptional(5)}');
+  
+  print(greetPerson(name: 'ferxxxo',message:'Un saludo a'));
 }
 
 greetEveryOne() => "Hello every one";
@@ -22,4 +24,9 @@ addTwoNumbers(int a , int b) => a+b;
 addTwoNumbersOptional(int a ,[int b = 5]) {
   
   return  a+b;
+}
+
+
+String greetPerson({required String name,String message= 'hola,'}){
+  return '$message $name';
 }
