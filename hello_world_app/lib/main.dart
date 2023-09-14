e2c0471e1f3da57692ec7e8b3d0a87a9b87700d0
Neonx <fabiangomes2015@gmail.com>
2023-09-14 13:07:17 -0500

    adding scaffold and center child to hello world

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(child: Text('Hola mundo')),
-    );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(child: Text('Hola mundo')),
+        ));
   }
 }
