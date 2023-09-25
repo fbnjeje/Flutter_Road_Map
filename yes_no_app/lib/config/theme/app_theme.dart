5e134b8c10525fd1ffdf2d6ee0904f22bd089c82
Neonx <fabiangomes2015@gmail.com>
2023-09-25 17:58:11 -0500

    Addign themes and colors for new app

--- yes_no_app/lib/config/theme/app_theme.dart
+++ yes_no_app/lib/config/theme/app_theme.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
+const Color _customColor = Color.fromARGB(169, 120, 255, 149);
+
+const List<Color> _colorThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+];
+
 class AppTheme {
   ThemeData theme() {
-    return ThemeData();
+    return ThemeData(useMaterial3: true, colorSchemeSeed: _colorThemes[6]);
   }
 }
