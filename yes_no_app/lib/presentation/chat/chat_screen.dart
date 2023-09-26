0bc53766d94e78295e36bf3367173994601484a5
Neonx <fabiangomes2015@gmail.com>
2023-09-26 13:56:15 -0500

    folder config.themes and archive chat screen

---
+++ yes_no_app/lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CharScreen extends StatelessWidget {
+  const CharScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mi amor 💗'),
+      ),
+    );
+  }
+}
