3b502b0388963f38d4c5e79abc9469553e1f72e1
Neonx <fabiangomes2015@gmail.com>
2023-09-28 15:08:48 -0500

    Generating Column on Chat view with Children

--- yes_no_app/lib/presentation/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/chat/chat_screen.dart
@@ -11,7 +11,7 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
             backgroundImage: NetworkImage(
-                'https://www.svg.com/img/gallery/why-princess-zelda-sounds-so-familiar-in-tears-of-the-kingdom/intro-1683545985.jpg'),
+                'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9f7c4355-9421-4e9a-9027-565efe5e6c20/db16yph-bb4d3da1-8282-43a3-b4d7-0d2075fcdf39.jpg/v1/fill/w_1192,h_670,q_70,strp/zelda_s_smile_by_zacharico_db16yph-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvOWY3YzQzNTUtOTQyMS00ZTlhLTkwMjctNTY1ZWZlNWU2YzIwXC9kYjE2eXBoLWJiNGQzZGExLTgyODItNDNhMy1iNGQ3LTBkMjA3NWZjZGYzOS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.pw9udnXgLFtlUunzFKyGdZco5-JMAp5t9RKB9AI9cnM'),
           ),
         ),
         title: const Text('Mi amor 💗'),
@@ -23,12 +23,10 @@ class ChatScreen extends StatelessWidget {
 }
 
 class _ChatView extends StatelessWidget {
-  const _ChatView({
-    super.key,
-  });
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Column(
+      children: [],
+    );
   }
 }
