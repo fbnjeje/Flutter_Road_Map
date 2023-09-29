ec60c6a712eb70b7c48632be3af31da3a4647994
Neonx <fabiangomes2015@gmail.com>
2023-09-28 19:11:36 -0500

    adding a styles to chat

--- yes_no_app/lib/presentation/Widgets/Chat/my_message_bubble.dart
+++ yes_no_app/lib/presentation/Widgets/Chat/my_message_bubble.dart
@@ -10,13 +10,16 @@ class MyMessageBubble extends StatelessWidget {
         Container(
           decoration: const BoxDecoration(color: Colors.black),
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Text(
               'lorem Ipmsun 12 123 hola hola 123',
               style: TextStyle(color: Colors.white),
             ),
           ),
         ),
+        const SizedBox(
+          height: 10,
+        )
       ],
     );
   }
