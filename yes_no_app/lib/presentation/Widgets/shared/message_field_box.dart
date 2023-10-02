18245e6098b4fc0071779ea7cf437c850ccc2600
Neonx <fabiangomes2015@gmail.com>
2023-10-02 13:28:42 -0500

    adding a new styles's chat form

--- yes_no_app/lib/presentation/Widgets/shared/message_field_box.dart
+++ yes_no_app/lib/presentation/Widgets/shared/message_field_box.dart
@@ -5,12 +5,14 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
+    final outlineInputBorder = UnderlineInputBorder(
+        borderSide: const BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(20));
 
     return TextFormField(
       decoration: InputDecoration(
-          enabledBorder:
-              OutlineInputBorder(borderSide: BorderSide(color: colors.primary)),
+          enabledBorder: outlineInputBorder,
+          focusedBorder: outlineInputBorder,
           filled: true,
           suffixIcon: IconButton(
             icon: const Icon(Icons.send_outlined),
