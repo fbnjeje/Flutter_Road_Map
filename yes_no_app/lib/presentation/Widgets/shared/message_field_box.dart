2d9d820e63b4efb4bff35a088a822f9e6c097117
Neonx <fabiangomes2015@gmail.com>
2023-10-02 13:16:52 -0500

    adding styles's  input textformfield

--- yes_no_app/lib/presentation/Widgets/shared/message_field_box.dart
+++ yes_no_app/lib/presentation/Widgets/shared/message_field_box.dart
@@ -5,6 +5,17 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Hola');
+    final colors = Theme.of(context).colorScheme;
+
+    return TextFormField(
+      decoration: InputDecoration(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: colors.primary)),
+          filled: true,
+          suffixIcon: IconButton(
+            icon: const Icon(Icons.send_outlined),
+            onPressed: () {},
+          )),
+    );
   }
 }
