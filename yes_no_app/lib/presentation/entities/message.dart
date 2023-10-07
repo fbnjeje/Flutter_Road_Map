3409e16425f9e6faed3b29780e371fc82138d0dd
Neonx <fabiangomes2015@gmail.com>
2023-10-06 20:31:54 -0500

    initalized Message class

--- yes_no_app/lib/presentation/entities/message.dart
+++ yes_no_app/lib/presentation/entities/message.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/material.dart';
+
 enum FromWho { mine, hers }
 
 class Message {
   final String text;
   final String? imageUrl;
+  final FromWho fromWho;
+
+  Message({required this.text, this.imageUrl, required this.fromWho});
 }
