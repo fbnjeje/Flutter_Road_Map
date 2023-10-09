5196d6c91a3ac4a04c40a3823097a908ac322b33
Neonx <fabiangomes2015@gmail.com>
2023-10-09 12:45:13 -0500

    learning about text controll and cleaning

--- yes_no_app/lib/presentation/Widgets/shared/message_field_box.dart
+++ yes_no_app/lib/presentation/Widgets/shared/message_field_box.dart
@@ -5,29 +5,34 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
+        hintText: 'End your message with a "??"',
         enabledBorder: outlineInputBorder,
         focusedBorder: outlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
           icon: const Icon(Icons.send_outlined),
           onPressed: () {
-            print('Valor de la caja de texto?');
+            final textValue = textController.value.text;
+            print('button: $textValue');
+            textController.clear();
           },
         ));
 
     return TextFormField(
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('Submit value $value');
+        textController.clear();
       },
-      onChanged: (value) {
-        print('changed $value');
-      },
+      onChanged: (value) {},
     );
   }
 }
