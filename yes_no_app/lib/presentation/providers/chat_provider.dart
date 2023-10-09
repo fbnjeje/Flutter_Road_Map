2ff572ad885b06207a501ba29f2c2eed6a7a7542
Neonx <fabiangomes2015@gmail.com>
2023-10-09 12:07:46 -0500

    throwing data form my bubble chats

--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: 'hola amor!', fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo???', fromWho: FromWho.me)
   ];
--- yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
 import 'package:yes_no_app/presentation/Widgets/Chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/Widgets/Chat/my_message_bubble.dart';
 import 'package:yes_no_app/presentation/Widgets/shared/message_field_box.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -28,6 +31,8 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(
@@ -37,9 +42,11 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
                 child: ListView.builder(
-              itemCount: 100,
+              itemCount: chatProvider.messageList.length,
               itemBuilder: (context, index) {
-                return (index % 2 == 0)
+                final message = chatProvider.messageList[index];
+
+                return (message.fromWho == FromWho.hers)
                     ? const HerMessageBubble()
                     : const MyMessageBubble();
               },
