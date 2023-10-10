9480b434d35cd570d5546c5ff3a2324cc3a52472
Neonx <fabiangomes2015@gmail.com>
2023-10-09 20:23:55 -0500

    adding her Reply and using the API

--- yes_no_app/lib/config/helpers/get_yes_no_answer.dart
+++ yes_no_app/lib/config/helpers/get_yes_no_answer.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
-class YesNoAnswer {
-  Future<Message> getAnswer() {
-    return;
+class GetYesNoAnswer {
+  final _dio = Dio();
+  Future<Message> getAnswer() async {
+    final response = await _dio.get('https://yesno.wtf/api');
+
+    throw UnimplementedError();
   }
 }
