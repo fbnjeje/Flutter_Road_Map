d6a43da31d03dc04efdee64837849e43e840d737
Neonx <fabiangomes2015@gmail.com>
2023-10-11 11:59:24 -0500

    counter click

--- Todo/to_do/lib/main.dart
+++ Todo/to_do/lib/main.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:to_do/presentation/screens/counter_screen.dart';
 
 void main() {
   runApp(const MyWidget());
 }
 
 class MyWidget extends StatelessWidget {
+  final counterNumber = 0;
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
-      home: const CounterScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu),
+            tooltip: 'Open Menu',
+          ),
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.account_circle_rounded)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.public_sharp))
+          ],
+          title: const Text(
+            'holagente',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        body: Center(
+          child: Text('Click $counterNumber'),
+        ),
+      ),
     );
   }
 }
