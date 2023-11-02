167c8a1a210fc5a2804174ecfdc5f3bf9be7ab9d
Neonx <fabiangomes2015@gmail.com>
2023-11-02 17:30:46 -0500

    adding an icon from assets

--- sushi app/sushi_app/lib/screens/intro_page.dart
+++ sushi app/sushi_app/lib/screens/intro_page.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: const Color.fromARGB(255, 138, 60, 55),
       body: Column(
         children: [
-          SizedBox(
-            height: 25,
+          const SizedBox(height: 25),
+          //shop Name
+          Text(
+            "KYOTO'S SUSHI",
+            style:
+                GoogleFonts.dmSerifDisplay(fontSize: 28, color: Colors.white),
           ),
-          Text("hola gente de yt")
+          //SPACE
+          const SizedBox(height: 25),
+
+          //icon
+
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Image.asset('lib/assets/sushi (1).png'),
+          )
         ],
       ),
     );
