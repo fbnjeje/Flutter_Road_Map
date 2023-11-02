8c0a085fbb9e3c092e875b118ad6cb1f456ceac4
Neonx <fabiangomes2015@gmail.com>
2023-11-02 18:05:52 -0500

    pushing title on the left

--- sushi app/sushi_app/lib/screens/intro_page.dart
+++ sushi app/sushi_app/lib/screens/intro_page.dart
@@ -8,31 +8,35 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 138, 60, 55),
-      body: Column(
-        children: [
-          const SizedBox(height: 25),
-          //shop Name
-          Text(
-            "KYOTO'S SUSHI",
-            style:
-                GoogleFonts.dmSerifDisplay(fontSize: 28, color: Colors.white),
-          ),
-          //SPACE
-          const SizedBox(height: 25),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 25),
+            //shop Name
+            Text(
+              "KYOTO'S SUSHI",
+              style:
+                  GoogleFonts.dmSerifDisplay(fontSize: 28, color: Colors.white),
+            ),
+            //SPACE
+            const SizedBox(height: 25),
 
-          //icon
+            //icon
 
-          Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Image.asset('lib/assets/sushi (2).png'),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.asset('lib/assets/sushi (2).png'),
+            ),
 
-          //title
+            //title
 
-          //subtitle
+            //subtitle
 
-          //getStarted button
-        ],
+            //getStarted button
+          ],
+        ),
       ),
     );
   }
