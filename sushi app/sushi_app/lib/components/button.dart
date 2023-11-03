64afc6067799066549d4710f67aa82544bd665c0
Neonx <fabiangomes2015@gmail.com>
2023-11-03 15:53:34 -0500

    add a Gesture detector on button widget

--- sushi app/sushi_app/lib/components/button.dart
+++ sushi app/sushi_app/lib/components/button.dart
@@ -6,24 +6,26 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 125, 77, 74),
-          borderRadius: BorderRadius.circular(40)),
-      padding: const EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(width: 20),
-          const Icon(
-            Icons.forward,
-            color: Colors.white,
-          )
-        ],
+    return GestureDetector(
+      child: Container(
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 125, 77, 74),
+            borderRadius: BorderRadius.circular(40)),
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(width: 20),
+            const Icon(
+              Icons.forward,
+              color: Colors.white,
+            )
+          ],
+        ),
       ),
     );
   }
