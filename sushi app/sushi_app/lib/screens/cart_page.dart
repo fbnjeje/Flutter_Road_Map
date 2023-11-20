e19c0598f341da3082b042e2149e458bc8c42152
Neonx <fabiangomes2015@gmail.com>
2023-11-19 20:10:13 -0500

    add working list on cart items

--- sushi app/sushi_app/lib/screens/cart_page.dart
+++ sushi app/sushi_app/lib/screens/cart_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sushi_app/models/food.dart';
+import 'package:sushi_app/models/shop.dart';
 import 'package:sushi_app/theme/colors.dart';
 
 class CartPage extends StatelessWidget {
@@ -6,12 +9,31 @@ class CartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cart Shop Section'),
-        backgroundColor: myPrimaryColor,
+    return Consumer<Shop>(
+      builder: (context, value, child) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Cart Shop Section'),
+          backgroundColor: myPrimaryColor,
+        ),
+        body: ListView.builder(
+          itemCount: value.cart.length,
+          itemBuilder: (context, index) {
+            //get food from cart
+            final Food food = value.cart[index];
+            //get name
+            final String foodName = food.name;
+
+            //get food price
+            final String price = food.price;
+            //return list title
+
+            return ListTile(
+              title: Text(foodName),
+              subtitle: Text(price),
+            );
+          },
+        ),
       ),
-      body: ListView.builder(itemBuilder: (context, index) {}),
     );
   }
 }
